a393bbf779c903888517a2c78087573ac4cc7dfc
Giovanni Oliveira <dmogiovanni@gmail.com>
2023-06-22 09:00:24 -0300

    Adicionando mensagens de inclusão e exclusão de série

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -10,27 +10,31 @@ class SeriesController extends Controller
 {
     public function index(Request $request){
 
-        $series = Serie::all();
-        
-        // return view('listar-series', [
-        //     'series' => $series
-        // ]);
-        return view('series.index')->with('series', $series);
+        $series = Serie::query()->orderBy('nome')->get();
+
+        $mensagemSucesso = session('mensagem.sucesso');      
+      
+        return view('series.index')->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
     }
 
     public function create(){ 
-      return view('series.create'); 
+      return view('series.create');
+
     } 
     
     public function store(Request $request){
       
       Serie::create($request->all());
+      $request->session()->flash('mensagem.sucesso', 'Série adicionada com sucesso.');
 
       return to_route('series.index');
     }
 
     public function destroy(Request $request){
       Serie::destroy($request->series);
+
+      $request->session()->flash('mensagem.sucesso', 'Série removida com sucesso.');
+
       return to_route('series.index');
     }
 }
