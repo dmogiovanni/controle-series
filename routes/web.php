940aebd1292dba5c788a94e2040f6ff579a9ef24
Giovanni Oliveira <dmogiovanin@gmail.com>
2023-06-24 15:06:09 -0300

    Adicionado pagina para listar episodios

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EpisodesController;
 use App\Http\Controllers\SeriesController;
 use App\Http\Controllers\SeasonsController;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,8 @@ Route::get('/', function () {
 Route::resource('/series', SeriesController::class)->except(['show']);
 
 Route::get('/series/{series}/season', [SeasonsController::class, 'index'])->name('seasons.index');
+
+Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
 
 // Route::delete('/series/destroy/{serie}', [SeriesController::class, 'destroy'])->name('series.destroy');
 
